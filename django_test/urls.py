0d8d55f410b1faf3a6890d8cb0f65064fa554520
jamespbrink <james.p.brink@gmail.com>
2014-10-02 16:33:51 -0400

    finished lecture 6

--- django_test/urls.py
+++ django_test/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from article.views import HelloTemplate
+admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^hello/$', 'article.views.hello'),
-	url(r'^hello_template/$', 'article.views.hello_template'),
-	url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
-	url(r'^hello_class_view/$', HelloTemplate.as_view()),
+	(r'^articles/', include('article.urls')),
     # Examples:
     # url(r'^$', 'django_test.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
